f0c00c34f5a53533e7d1bb40b8b55d862ca56d5f
Romano Livi <romano_livi@outlook.com>
2020-12-15 18:09:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-
+contacts["Freddy Mercury"].each do |person, data|
+  
+  data.each do |attribute, value|
+    if value == favorite_ice_cream_flavors
+      delete.if?("strawberry")
+    end
+  end
+end
 end
